52bdde22ed47a3207caf1b7a60063758c270c710
Build Tools <build.tools@example.com>
2021-04-24 20:02:05 +0100

    tested, working

--- nano_ps2_pedal_interface/nano_ps2_pedal_interface.ino
+++ nano_ps2_pedal_interface/nano_ps2_pedal_interface.ino
@@ -1,30 +1,34 @@
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
- This example code is in the public domain.
- */
-
 #include "string.h"
 
 #define BAUD_RATE 57600
 #define LOOP_RATE_HZ 500
 
-// the setup routine runs once when you press reset:
+const int pedal_1_upper = 920;
+const int pedal_1_lower = 560;
+const int pedal_2_upper = 1023;
+const int pedal_2_lower = 740;
+
+const int output_upper = 1024;
+const int output_lower = 0;
+
+boolean flip_output = true;
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(BAUD_RATE);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int pedal_value_1 = analogRead(A6);
-  int pedal_value_2 = analogRead(A7);  
-  // print out the value you read:
+  int pedal_value_1 = 0;
+  int pedal_value_2 = 0;
+  if (!flip_output){
+    pedal_value_1 = map(analogRead(A4), pedal_1_lower, pedal_1_upper, output_lower, output_upper);
+    pedal_value_2 = map(analogRead(A6), pedal_2_lower, pedal_2_upper, output_lower, output_upper);
+  } else {
+    pedal_value_1 = map(analogRead(A4), pedal_1_lower, pedal_1_upper, output_upper, output_lower);
+    pedal_value_2 = map(analogRead(A6), pedal_2_lower, pedal_2_upper, output_upper, output_lower);
+  }
   String string_out;
-  string_out = "a" + String(pedal_value_1, DEC) + "b" + String(pedal_value_2, DEC);
+  string_out = String(pedal_value_1, DEC) + "b" + String(pedal_value_2, DEC);
   Serial.println(string_out);
   delay(LOOP_RATE_HZ/2);        // delay in between reads for stability
 }
