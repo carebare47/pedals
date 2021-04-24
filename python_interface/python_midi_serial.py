a0faefe5efb2ba2adcc0111e1aa9a348a4789c28
carebare47 <tom_q@hotmail.co.uk>
2021-04-25 00:54:05 +0100

    left too

--- python_interface/python_midi_serial.py
+++ python_interface/python_midi_serial.py
@@ -24,13 +24,22 @@ deadband=30
 def _map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
-
+last_reading = 0
+# time_limit_ms = 
 while True:
 	message = arduino.readline()[:-2]
 	i = i + 1
 	if message:
-		left = message.split("b")[1]
-	        right = message.split("b")[0]
+		left = int(message.split("b")[1])
+	        right = int(message.split("b")[0])
+		if left < 0:
+			left = 0
+		if left > 1024:
+			left = 1024
+		if right < 0:
+			right = 0
+		if right > 1024:
+			right = 1024
 		print "left: " + str(left) + "\tright: " + str(right)
-		if left > deadband:
-			midi_out.send_message([0xB0, 7, _map(left, 0, 1024, 0, 127)]) # volume
+		midi_out.send_message([0xB0, 7, _map(int(left), 0, 1024, 127, 0)]) # volume
+		midi_out.send_message([0xB0, 43, _map(int(right), 0, 1024, 30, 127)])
