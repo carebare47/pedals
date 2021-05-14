57cfb2392090073a8d841fa77f6c8c0c65ac7690
carebare47 <tom@shadowrobot.com>
2021-05-14 20:58:48 +0100

    start of looper

---
+++ python_interface/midi_looper.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+import sys
+import serial
+from serial.serialutil import SerialException
+import time
+import rtmidi
+
+
+midi_out = rtmidi.MidiOut()
+midi_out.open_port(1)
+
+'''
+0: Sysex (13 bytes)
+    F0 41 10 00 00 72 12 00 00 00 08 78 F7 
+1: Sysex (13 bytes)
+    F0 41 10 00 00 72 12 00 00 00 08 78 F7 
+2: Sysex (13 bytes)
+    F0 41 10 00 00 72 12 00 00 00 08 78 F7 
+3: Sysex (13 bytes)
+    F0 41 10 00 00 72 12 00 00 00 08 78 F7 
+4: Sysex (13 bytes)
+    F0 41 10 00 00 72 12 00 00 00 08 78 F7 
+'''
+
+bpm = 120
+bps = bpm/60
+
+class PyLooper():
+        def __init__(self, bpm=120, time_sig=4):
+                self.bpm = bpm
+                self.bps = bpm/60.0
+                self._valid_time_sigs = [2, 3, 4]
+                if time_sig not in self._valid_time_sigs:
+                        print("uh oh time sig not valid")
+                        raise
+                self.time_sig = time_sig
+                self.current_time = time.time()
+                self.bar_length_seconds = self.bps * time_sig
+                self.last_remainder = 0
+                while True:
+                        self.loop()
+
+        def loop(self):
+                now = time.time()
+                while (now + self.bar_length_seconds) > time.time():
+                        self.record((now + self.bar_length_seconds) - time.time())
+                print("NEW BAH")
+
+        def record(self, t_time):
+                current_beat = math.ceil((self.bar_length_seconds - t_time) / self.bps)
+                remainder = math.floor((self.bar_length_seconds - t_time) % self.bps)
+                if remainder != self.last_remainder:
+                        print(current_beat, remainder, self.last_remainder)
+                        self.last_remainder = remainder
+
+
+loop = PyLooper()
+
+arduino = serial.Serial('/COM4', 57600, timeout=.1)
+
+
+i=0
+deadband=30
+def _map(x, in_min, in_max, out_min, out_max):
+	return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+tflag = False
+last_reading = 0
+last_l = 0
+last_r = 0
+# time_limit_ms = 
+while True:
+	message = arduino.readline()[:-2]
+	i = i + 1
+	if message:
+		left = int(message.split(b'b')[1])
+		right = int(message.split(b'b')[0])
+		if left < 0:
+			left = 0
+		if left > 1024:
+			left = 1024
+		if right < 0:
+			right = 0
+		if right > 1024:
+			right = 1024
+		if right > 20 and right == last_r:
+                        right = right - 1
+		if left > 20 and left == last_l:
+                        left = left - 1
+		print("left: " + str(left) + "\tright: " + str(right))
+		midi_out.send_message([0xB0, 44, _map(int(left), 0, 1024, 0, 120)]) # volume
+		midi_out.send_message([0xB0, 43, _map(int(right), 0, 1024, 35, 105)])
+		last_l = left
+		last_r = right
+		if not time.localtime(time.time())[5] % 10 and not tflag:
+			print(time.asctime(time.localtime(time.time())))
+			tflag = True
